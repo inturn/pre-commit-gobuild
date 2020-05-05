7a2002a5869c8c30ca59aa3aa0e6064cc57ae414
Yauheni Ramanovich <sweetsprite4@gmail.com>
2020-05-05 15:09:18 +0300

    BE-3842 go-imports fire os.Exit(1) if contents have changed (#7)
    * BE-3842 go-imports fire os.Exit(1) if contents have changed

--- hooks/go-imports/go-imports.go
+++ hooks/go-imports/go-imports.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"flag"
+	"fmt"
 	"go/ast"
 	"go/format"
 	"go/parser"
@@ -16,6 +19,21 @@ import (
 	"github.com/inturn/pre-commit-gobuild/internal/helpers"
 )
 
+type lintError struct {
+	err  error
+	path string
+}
+
+var (
+	errNotSorted = fmt.Errorf("file imports have to be sorted or formatted according to go.fmt")
+	checkOnly    bool
+)
+
+func init() {
+	flag.BoolVar(&checkOnly, "check-only", false, "linter only checks imports order and don't modify files")
+	flag.Parse()
+}
+
 func main() {
 	workDir, err := os.Getwd()
 	if err != nil {
@@ -23,55 +41,99 @@ func main() {
 	}
 	dirs := helpers.DirsWith(workDir, "\\.go$")
 
+	errc := make(chan lintError, 10)
 	wg := &sync.WaitGroup{}
 
-	for _, dir := range dirs {
-		if !strings.Contains(dir, "/vendor/") {
-			files, err := ioutil.ReadDir(dir)
-			if err != nil {
-				log.Printf("error occured on read dir %s: %s", dir, err)
-			}
-			for _, f := range files {
-				if !strings.HasSuffix(f.Name(), ".go") {
-					continue
+	go func() {
+		for _, dir := range dirs {
+			if !strings.Contains(dir, "/vendor/") {
+				files, err := ioutil.ReadDir(dir)
+				if err != nil {
+					log.Printf("error occured on read dir %s: %s", dir, err)
 				}
-				wg.Add(1)
-				go func(d, name string) {
-					sortFileImports(filepath.Join(d, name))
-					wg.Done()
-				}(dir, f.Name())
+				for _, f := range files {
+					if !strings.HasSuffix(f.Name(), ".go") {
+						continue
+					}
+					wg.Add(1)
+					go func(d, name string) {
+						sortFileImports(filepath.Join(d, name), errc)
+						wg.Done()
+					}(dir, f.Name())
+				}
 			}
 		}
+		wg.Wait()
+		close(errc)
+	}()
+
+	var le []lintError
+	for lintErr := range errc {
+		if !checkOnly && lintErr.err == errNotSorted {
+			continue
+		}
+		log.Println(lintErr.path, lintErr.err)
+		le = append(le, lintErr)
 	}
-	wg.Wait()
+	if len(le) != 0 {
+		log.Println("errors count:", len(le))
+		os.Exit(1)
+	}
 }
 
-func sortFileImports(path string) {
+func sortFileImports(path string, errc chan<- lintError) {
 	fSet := token.NewFileSet()
 
 	f, err := parser.ParseFile(fSet, path, nil, parser.ParseComments)
 	if err != nil {
-		log.Println(err)
+		errc <- lintError{
+			err:  err,
+			path: path,
+		}
 		return
 	}
 
 	sortImports(f)
 
-	file, err := os.OpenFile(path, os.O_RDWR, 0666)
+	buf := &bytes.Buffer{}
+	if err := format.Node(buf, fSet, f); err != nil {
+		errc <- lintError{
+			err:  err,
+			path: path,
+		}
+		return
+	}
+
+	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		log.Println(err)
+		errc <- lintError{
+			err:  err,
+			path: path,
+		}
 		return
 	}
 
-	if err := file.Truncate(0); err != nil {
-		log.Println(err)
+	if buf.String() == string(data) {
 		return
 	}
 
-	if err := format.Node(file, fSet, f); err != nil {
-		log.Println(err)
+	if checkOnly {
+		errc <- lintError{
+			err:  errNotSorted,
+			path: path,
+		}
 		return
 	}
+
+	if err := ioutil.WriteFile(path, buf.Bytes(), 0664); err != nil {
+		errc <- lintError{
+			err:  err,
+			path: path,
+		}
+		return
+	}
+
+	log.Printf("%s file has changed", path)
 }
 
 func sortImports(f *ast.File) {
@@ -85,7 +147,7 @@ func sortImports(f *ast.File) {
 	for _, imp := range f.Imports {
 		impData := importData{}
 
-		if imp.Doc != nil  && imp.Name != nil && imp.Name.Name == "_" {
+		if imp.Doc != nil && imp.Name != nil && imp.Name.Name == "_" {
 			impData.comment = imp.Doc.Text()
 		}
 
@@ -103,10 +165,9 @@ func sortImports(f *ast.File) {
 		imp1 = append(imp1, impData)
 	}
 
-
 	nonImportComment := f.Comments[:0]
 	startPos := f.Imports[0].Pos()
-	lastPos := f.Imports[len(f.Imports) - 1].End()
+	lastPos := f.Imports[len(f.Imports)-1].End()
 
 	for _, c := range f.Comments {
 		if c.Pos() > lastPos || c.Pos() < startPos {
@@ -150,14 +211,14 @@ func sortImports(f *ast.File) {
 	}
 }
 
-func addISpec(imp importData, d *ast.GenDecl)  {
+func addISpec(imp importData, d *ast.GenDecl) {
 	if imp.name == "_" {
 		comm := imp.comment
 		if comm == "" {
 			comm = "todo comment here why do you use blank import"
 		}
 		d.Specs = append(d.Specs, &ast.ImportSpec{
-			Path: &ast.BasicLit{ Value: "// " + strings.TrimSpace(comm) },
+			Path: &ast.BasicLit{Value: "// " + strings.TrimSpace(comm)},
 		})
 	}
 	iSpec := ast.ImportSpec{
@@ -170,8 +231,8 @@ func addISpec(imp importData, d *ast.GenDecl)  {
 type impSlice []importData
 
 type importData struct {
-	value string
-	name  string
+	value   string
+	name    string
 	comment string
 }
 
